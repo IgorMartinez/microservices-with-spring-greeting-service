930eb49ffb3b15adca5c691d84eabbe92708396e
Igor Martinez <igorgabriel.martinez@gmail.com>
2023-09-26 13:51:22 -0300

    Refactor greeting controller

--- src/main/java/br/com/igormartinez/greetingservice/greeting/GreetingController.java
+++ src/main/java/br/com/igormartinez/greetingservice/greeting/GreetingController.java
@@ -4,10 +4,12 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
     
     private static final String template = "%s, %s!";
@@ -16,7 +18,7 @@ public class GreetingController {
 	@Autowired
 	private GreetingConfiguration configuration;
 
-	@GetMapping("/greeting")
+	@GetMapping
 	public Greeting greeting(@RequestParam(value="name",defaultValue = "") String name) {
 
 		if (name.isEmpty()) name = configuration.getDefaultValue();
